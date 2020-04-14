0b397affa33142aba44fe8fba57b88ea04caee22
Johnson Chen <54852329+c-yue@users.noreply.github.com>
2020-04-15 04:12:20 +0800

    update comment of the best function

--- best_func.R
+++ best_func.R
@@ -1,3 +1,8 @@
+## a function called best that take two arguments: the 2-character abbreviated name of a state and an
+## outcome name. The function reads the outcome-of-care-measures.csv file and returns a character vector
+## with the name of the hospital that has the best (i.e. lowest) 30-day mortality for the specified outcome
+## in that state.
+
 best <- function(state = "TX", outcome = "heart attack")
 {
         ## Read data
